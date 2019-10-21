58a18bf9892a9b1ffe3de2d6d8442c058171327b
C.Rondeau <rondeau.clement@gmail.com>
2019-10-21 22:02:00 +0200

    + collision between actors

---
+++ Assets/ActorCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class ActorCollider : MonoBehaviour
+{
+    [SerializeField] private LayerMask m_collisionLayers;
+
+    private void OnTriggerEnter2D(Collider2D p_other)
+    {
+        if (m_collisionLayers == (m_collisionLayers | (1 << p_other.gameObject.layer)))
+        {
+            p_other.GetComponent<HealthBehaviour>().TakeDamage(5);
+            gameObject.GetComponent<HealthBehaviour>().TakeDamage(5);
+        }
+    }
+}
